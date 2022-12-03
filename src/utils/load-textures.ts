ba365e4c49cbd504c593ae7e7d08cec5d7caf81a
nionoku <nionoku@gmail.com>
2022-12-03 23:28:58 +0600

    Add tokens texture

---
+++ src/utils/load-textures.ts
@@ -0,0 +1,16 @@
+import { LoadingManager, Texture, TextureLoader } from "three";
+import textures from '../assets/textures.json'
+
+export async function loadTextures(loadingManager?: LoadingManager): Promise<Record<keyof typeof textures, Texture>> {
+  const textureLoader = new TextureLoader(loadingManager)
+  const resourcesData = Object.entries(textures).map(async it => ({
+    name: it[0],
+    texture: await textureLoader.loadAsync(it[1])
+  }))
+  const resolvedResourcesData = await Promise.all(resourcesData)
+
+  return resolvedResourcesData.reduce((acc, it) => ({
+    ...acc,
+    [it.name]: it.texture
+  }), {} as Record<keyof typeof textures, Texture>)
+}

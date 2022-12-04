a0d5afa42ece66db8a7f9cd622b578c028f0ee65
nionoku <nionoku@gmail.com>
2022-12-04 19:49:57 +0600

    Refactoring

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,9 @@ import { Color, LoadingManager, PerspectiveCamera, Vec2, WebGLRenderer } from 't
 import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass'
 import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer'
 import Tween from '@tweenjs/tween.js'
-import boardDescription from './assets/boards/board_1.json'
-import boardTokensDescription from './assets/boards/board_1_tokens.json'
-import robotsDescription from './assets/robots.json'
+import bd from './assets/boards/board_1.json'
+import btd from './assets/boards/board_1_tokens.json'
+import rd from './assets/robots.json'
 import { loadTextures } from './utils/load-textures'
 import { loadStlModels } from './utils/load-models'
 import { Robot } from './models/robot'
@@ -19,145 +19,185 @@ import { BoardController } from './controller/board'
 import { RobotsController } from './controller/robots'
 import { RoundController } from './controller/round'
 
-const scene = new SceneController()
-const camera = new PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 1000)
-const renderer = new WebGLRenderer({
-  antialias: true,
-})
-const composer = new EffectComposer(renderer)
-const renderPass = new RenderPass(scene, camera)
-const controls = new CameraController(camera, renderer.domElement)
-const loadingManager = new LoadingManager()
-const raycaster = new RaycasterController()
-
-const textures = await loadTextures(loadingManager)
-const models = await loadStlModels(loadingManager)
-const arrow = await new SVGLoader(loadingManager)
-  .loadAsync('/arrow.svg')
-  // arrow.svg contains only 1 <path>
-  .then(it => it.paths[0].toShapes(true))
-  .then(it => new Arrow(it))
-
-const arrowsController = new ArrowsController(arrow)
-const boardController = new BoardController(boardDescription, boardTokensDescription, textures)
-const robotsController = new RobotsController().make(robotsDescription, models)
-const roundController = new RoundController(boardController.tokens.slice())
-const gameController = new GameController(boardController, robotsController, arrowsController, roundController)
-
-// end of round listener (after target robot place on target token)
-roundController.whenEndRound(() => {
-  alert('Win!')
-})
-// end of game listener (tokens ends)
-roundController.whenEndGame(() => {
-  alert('End of game!')
-})
-// change turn listener
-roundController.whenChangeTurn((turn) => {
-  alert('Turn: ' + turn)
-})
-
-// keypress listener
-window.addEventListener('keyup', (event) => {
-  switch (event.code) {
-    case 'ArrowLeft': {
-      if (gameController.hasSelectedRobot) {
-        gameController.moveSelectedRobot(0)
-      }
-
-      break
-    }
-      
-
-    case 'ArrowUp': {
-      if (gameController.hasSelectedRobot) {
-        gameController.moveSelectedRobot(1)
-      }
-      
-      break
-    }
-
-    case 'ArrowRight': {
-      if (gameController.hasSelectedRobot) {
-        gameController.moveSelectedRobot(2)
-      }
-      
-      break
-    }
-
-    case 'ArrowDown': {
-      if (gameController.hasSelectedRobot) {
-        gameController.moveSelectedRobot(3)
-      }
-      
-      break
-    }
-
-    case 'Space': {
-      // change background color
-      scene.changeBackground(new Color(robotsController.nextRobot.userData.tint))
-      controls.toInitialPosition()
-
-      gameController.setNextSelectedRobot()
-
-      break
-    }
-  }
-})
-// click listener
-renderer.domElement.addEventListener('click', (event) => {
-  const point: Vec2 = { x: event.clientX, y: event.clientY }
-  const intersects = raycaster.intersects(scene, camera, point, renderer.domElement)
+class ViewController {
+  private scene = new SceneController()
+  private camera = new PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 1000)
+  private renderer = new WebGLRenderer({
+    antialias: true,
+  })
+  private composer = new EffectComposer(this.renderer)
+  private renderPass = new RenderPass(this.scene, this.camera)
+  private controls = new CameraController(this.camera, this.renderer.domElement)
+  private raycaster = new RaycasterController()
+  private loadingManager = new LoadingManager()
   
-  if (!intersects.length) {
-    return
+  private arrows!: ArrowsController
+  private board!: BoardController
+  private robots!: RobotsController
+  private rc!: RoundController
+  private gc!: GameController
+
+  private clickListener = (event: MouseEvent) => {
+    const point: Vec2 = { x: event.clientX, y: event.clientY }
+    const intersects = this.raycaster.intersects(this.scene, this.camera, point, this.renderer.domElement)
+    
+    if (!intersects.length) {
+      return
+    }
+
+    // handle click by objects
+    let target
+
+    if ((target = intersects.find(it => it.object instanceof Arrow && it.object.visible))) {
+      this.beforeClickByArrow()
+      return this.gc.clickByArrow(target.object as Arrow)
+    }
+
+    if ((target = intersects.find(it => it.object instanceof Robot && it.object.visible))) {
+      this.beforeClickByRobot(target.object as Robot)
+      return this.gc.clickByRobot(target.object as Robot)
+    }
+
+    this.beforeMissClick()
+    return this.gc.clickMiss()
   }
 
-  controls.enabled = false
+  private keyupListener = (event: KeyboardEvent) => {
+    switch (event.code) {
+      case 'ArrowLeft': {
+        if (this.gc.hasSelectedRobot) {
+          this.gc.moveSelectedRobot(0)
+        }
 
-  // handle click by objects
-  let target
+        break
+      }
+        
 
-  if ((target = intersects.find(it => it.object instanceof Arrow && it.object.visible))) {
-    return gameController.clickByArrow(target.object as Arrow)
+      case 'ArrowUp': {
+        if (this.gc.hasSelectedRobot) {
+          this.gc.moveSelectedRobot(1)
+        }
+        
+        break
+      }
+
+      case 'ArrowRight': {
+        if (this.gc.hasSelectedRobot) {
+          this.gc.moveSelectedRobot(2)
+        }
+        
+        break
+      }
+
+      case 'ArrowDown': {
+        if (this.gc.hasSelectedRobot) {
+          this.gc.moveSelectedRobot(3)
+        }
+        
+        break
+      }
+
+      case 'Space': {
+        this.beforeClickByRobot(this.robots.nextRobot)
+        this.gc.setNextSelectedRobot()
+
+        break
+      }
+    }
   }
 
-  if ((target = intersects.find(it => it.object instanceof Robot && it.object.visible))) {
-    // change background color
-    scene.changeBackground(new Color(target.object.userData.tint))
-    controls.toInitialPosition()
+  public async make() {
+    const [textures, models, arrow] = await Promise.all([
+      loadTextures(this.loadingManager),
+      loadStlModels(this.loadingManager),
+      new SVGLoader(this.loadingManager)
+        .loadAsync('/arrow.svg')
+        // arrow.svg contains only 1 <path>
+        .then(it => it.paths[0].toShapes(true))
+        .then(it => new Arrow(it))
+    ])
 
-    return gameController.clickByRobot(target.object as Robot)
+    this.arrows = new ArrowsController(arrow)
+    this.board = new BoardController(bd, btd, textures)
+    this.robots = new RobotsController().make(rd, models)
+    this.rc = new RoundController(this.board.tokens.slice())
+    this.gc = new GameController(this.board, this.robots, this.arrows, this.rc)
+
+    this.prepare()
+    this.makeListeners()
   }
 
-  scene.changeBackground()
-  controls.enabled = true
-  return gameController.clickMiss()
-})
+  public start() {
+    const animate: XRFrameRequestCallback = (time: number) => {
+      this.composer.render()
+      this.controls.update()
+      Tween.update(time)
+    }
+    
+    // dom
+    document.body.prepend(this.renderer.domElement)
+    // start rendering
+    this.renderer.setAnimationLoop(animate)
+  }
 
-scene.add(
-  ...gameController.models
-)
+  private prepare() {
+    this.scene.add(
+      ...this.gc.models
+    )
+    // renderer
+    this.renderer.setPixelRatio(window.devicePixelRatio)
+    this.renderer.setSize(window.innerWidth, window.innerHeight)
+    // resize window listener
+    window.addEventListener('resize', () => {
+      this.renderer.setSize(window.innerWidth, window.innerHeight)
+      this.camera.aspect = window.innerWidth / window.innerHeight
+      this.camera.updateProjectionMatrix()
+    })
+    // composer
+    this.composer.addPass(this.renderPass)
+  }
 
-// renderer
-renderer.setPixelRatio(window.devicePixelRatio)
-renderer.setSize(window.innerWidth, window.innerHeight)
-// resize window listener
-window.addEventListener('resize', () => {
-  renderer.setSize(window.innerWidth, window.innerHeight)
-  camera.aspect = window.innerWidth / window.innerHeight
-  camera.updateProjectionMatrix()
-})
-// composer
-composer.addPass(renderPass)
+  private makeListeners() {
+    // end of round listener (after target robot place on target token)
+    this.rc.whenEndRound(() => {
+      alert('Win!')
+    })
+    // end of game listener (tokens ends)
+    this.rc.whenEndGame(() => {
+      alert('End of game!')
+    })
+    // change turn listener
+    this.rc.whenChangeTurn((turn) => {
+      alert('Turn: ' + turn)
+    })
 
-const animate: XRFrameRequestCallback = (time: number) => {
-  composer.render()
-  controls.update()
-  Tween.update(time)
+    // keypress listener
+    window.addEventListener('keyup', this.keyupListener)
+    // click listener
+    this.renderer.domElement.addEventListener('click', this.clickListener)
+  }
+
+  private beforeMissClick() {
+    this.controls.enabled = true
+    this.scene.changeBackground()
+  }
+
+  private beforeClickByArrow() {
+    this.controls.enabled = false
+  }
+  
+  private beforeClickByRobot(robot: Robot) {
+    this.controls.enabled = false
+    this.controls.toInitialPosition()
+    this.scene.changeBackground(new Color(robot.userData.tint))
+  }
 }
 
-// dom
-document.body.prepend(renderer.domElement)
-// start rendering
-renderer.setAnimationLoop(animate)
+try {
+  const vc = new ViewController()
+  await vc.make()
+  
+  vc.start()
+} catch (err) { 
+  throw new Error('Not implemented')
+}
